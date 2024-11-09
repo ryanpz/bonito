650a82cd943a17d0332ac10a7e48ff284167a599
Ryan Perez <ryan@rypz.io>
2024-11-09 13:23:03 -0800

    feat: rebase to ublue images and rename project to bonito (#5)
    * refactor: reduce copy layers
    
    * feat: rebase to ublue images and rename project
    
    * feat: add framework firmware and kernel modules
    
    * docs: update README

--- Containerfile
+++ Containerfile
@@ -1,20 +1,20 @@
-ARG FEDORA_ATOMIC_SPIN=sway-atomic
-ARG BASE_IMAGE=quay.io/fedora-ostree-desktops/${FEDORA_ATOMIC_SPIN}
-ARG FEDORA_MAJOR_VERSION=39
+ARG UBLUE_VARIANT=sericea-main
+ARG BASE_IMAGE=ghcr.io/ublue-os/${UBLUE_VARIANT}
+ARG FEDORA_MAJOR_VERSION=41
+
+FROM scratch AS ctx
+COPY ./ /
 
 FROM ${BASE_IMAGE}:${FEDORA_MAJOR_VERSION}
 
-ARG FEDORA_ATOMIC_SPIN
+ARG UBLUE_VARIANT
+ARG FEDORA_MAJOR_VERSION
 
-COPY files/system/etc /etc
-COPY files/system/usr /usr
+# contains framework kernel modules
+COPY --from=ghcr.io/ublue-os/akmods:main-${FEDORA_MAJOR_VERSION} /rpms/ /tmp/rpms
+RUN find /tmp/rpms
 
-COPY files/signing/policy.json /usr/etc/containers/policy.json
-COPY files/signing/registry-config.yaml /etc/containers/registries.d/ryanpz.yaml
-COPY cosign.pub /etc/pki/containers/ryanpz.pub
-
-COPY build.sh packages.json /tmp/ryanpz/
-
-RUN /tmp/ryanpz/build.sh /tmp/ryanpz/packages.json && \
-    rpm-ostree cleanup -m && \
+RUN --mount=type=cache,dst=/var/cache/rpm-ostree \
+    --mount=type=bind,from=ctx,source=/,target=/ctx \
+    /ctx/build.sh  && \
     ostree container commit
--- build.sh
+++ build.sh
@@ -2,12 +2,16 @@
 
 set -eu
 
-if [ ! -f "$1" ]; then
-    printf 'error: packages_file %s does not exist\n' "$1"
-    exit 1
-fi
+mkdir -p /usr/etc/containers \
+    /etc/containers/registries.d \
+    /etc/pki/containers
 
-jq_args="-r '.all.{{X}}[], .\"$FEDORA_ATOMIC_SPIN\".{{X}}[]' $1"
+cp -r /ctx/files/system/etc/* /etc
+cp /ctx/files/signing/policy.json /usr/etc/containers/policy.json
+cp /ctx/files/signing/registry-config.yaml /etc/containers/registries.d/ryanpz.yaml
+cp /ctx/cosign.pub /etc/pki/containers/ryanpz.pub
+
+jq_args="-r '.all.{{X}}[], .\"$UBLUE_VARIANT\".{{X}}[]' /ctx/packages.json"
 package_list() {
     echo "$jq_args" | sed "s/{{X}}/$1/g" | xargs jq
 }
@@ -19,6 +23,16 @@ include_list=$(package_list 'include')
 [ -n "$exclude_list" ] && rpm-ostree override remove $exclude_list
 # shellcheck disable=SC2086
 [ -n "$include_list" ] && rpm-ostree install $include_list
-systemctl enable rpm-ostreed-automatic.timer
-systemctl enable flatpak-system-update.timer
-systemctl --global enable flatpak-user-update.timer
+
+# framework kernel modules
+rpm-ostree install /tmp/rpms/kmods/*framework-laptop*.rpm
+
+# framework firmware
+# from https://github.com/ublue-os/bluefin/blob/f833e1f6a5d1863b26e6f24a5ec28068d511b3de/build_files/base/08-firmware.sh
+mkdir -p /tmp/mediatek-firmware
+curl -Lo /tmp/mediatek-firmware/WIFI_MT7922_patch_mcu_1_1_hdr.bin https://gitlab.com/kernel-firmware/linux-firmware/-/raw/8f08053b2a7474e210b03dbc2b4ba59afbe98802/mediatek/WIFI_MT7922_patch_mcu_1_1_hdr.bin?inline=false
+curl -Lo /tmp/mediatek-firmware/WIFI_RAM_CODE_MT7922_1.bin https://gitlab.com/kernel-firmware/linux-firmware/-/raw/8f08053b2a7474e210b03dbc2b4ba59afbe98802/mediatek/WIFI_RAM_CODE_MT7922_1.bin?inline=false
+xz --check=crc32 /tmp/mediatek-firmware/WIFI_MT7922_patch_mcu_1_1_hdr.bin
+xz --check=crc32 /tmp/mediatek-firmware/WIFI_RAM_CODE_MT7922_1.bin
+mv -vf /tmp/mediatek-firmware/* /usr/lib/firmware/mediatek/
+rm -rf /tmp/mediatek-firmware
